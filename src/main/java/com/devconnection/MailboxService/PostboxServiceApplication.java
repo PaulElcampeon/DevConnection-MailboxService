84e0ba6fe91418bcc5ec0869a4a89927b6e47aa6
PaulElcampeon <pauloladele@live.co.uk>
2019-08-27 22:28:46 +0100

    application entry

---
+++ src/main/java/com/devconnection/MailboxService/PostboxServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devconnection.MailboxService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostboxServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostboxServiceApplication.class, args);
+	}
+
+}
