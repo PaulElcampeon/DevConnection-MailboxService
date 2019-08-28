53237fc931149f85bf65ee964cd5d35dbe85e9a1
Paul Oladele <paul.oladele@gamesys.co.uk>
2019-08-28 12:41:34 +0100

    added  @Requestbody anotation

--- src/main/java/com/devconnection/MailboxService/controllers/PostboxServiceController.java
+++ src/main/java/com/devconnection/MailboxService/controllers/PostboxServiceController.java
@@ -1,4 +1,40 @@
 package com.devconnection.MailboxService.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devconnection.MailboxService.messages.DeleteMailMessage;
+import com.devconnection.MailboxService.messages.GenericResponse;
+import com.devconnection.MailboxService.messages.GetPostboxMessage;
+import com.devconnection.MailboxService.messages.GetPostboxResponse;
+import com.devconnection.MailboxService.messages.SendMailMessage;
+import com.devconnection.MailboxService.services.PostboxService;
+
+@RestController
 public class PostboxServiceController {
+
+    private PostboxService postboxService;
+
+    @Autowired
+    public PostboxServiceController(PostboxService postboxService) {
+        this.postboxService = postboxService;
+    }
+
+    @RequestMapping(value = "/postbox-service/get", method = RequestMethod.POST)
+    GetPostboxResponse getPostbox(@RequestBody GetPostboxMessage getPostboxMessage) {
+        return postboxService.getPostbox(getPostboxMessage);
+    }
+
+    @RequestMapping(value = "/postbox-service/send", method = RequestMethod.POST)
+    public void sendMailMessage(@RequestBody  SendMailMessage sendMailMessage) {
+        postboxService.sendMailMessage(sendMailMessage);
+    }
+
+    @RequestMapping(value = "/postbox-service/delete", method = RequestMethod.POST)
+    GenericResponse deleteMail(@RequestBody  DeleteMailMessage deleteMailMessage) {
+        return new GenericResponse(postboxService.deleteMail(deleteMailMessage));
+    }
 }
