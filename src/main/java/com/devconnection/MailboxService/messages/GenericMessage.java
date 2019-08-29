c15aa22ae02622d8ff31dfe6666d91aaa566707a
Paul Oladele <paul.oladele@gamesys.co.uk>
2019-08-29 16:24:20 +0100

    to be used in create method handler

---
+++ src/main/java/com/devconnection/MailboxService/messages/GenericMessage.java
@@ -0,0 +1,13 @@
+package com.devconnection.MailboxService.messages;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GenericMessage {
+
+    private String email;
+}
