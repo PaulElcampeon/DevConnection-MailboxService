c0a3c28b0774829eeef0ba06576f680df40add7c
PaulElcampeon <pauloladele@live.co.uk>
2019-09-04 06:49:42 +0100

    removed @CrossOrigin

--- src/main/java/com/devconnection/MailboxService/controllers/PostboxServiceController.java
+++ src/main/java/com/devconnection/MailboxService/controllers/PostboxServiceController.java
@@ -1,10 +1,7 @@
 package com.devconnection.MailboxService.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.devconnection.MailboxService.messages.DeleteMailMessage;
 import com.devconnection.MailboxService.messages.GenericMessage;
